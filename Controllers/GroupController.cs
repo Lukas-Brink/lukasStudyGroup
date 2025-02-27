c367e70b3141a47001db3a9f2b3be6c9525fb1d7
Lukas <Lukas@LUKASLAPTOP>
2025-02-27 19:44:47 +0200

    Add project files.

---
+++ Controllers/GroupController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Web.Mvc;
+
+namespace lukasStudyGroup.Controllers
+{
+    public class GroupController : Controller
+    {
+
+        public ActionResult ListMembers()
+        {
+            List<Models.GroupModel> members = new List<Models.GroupModel>();
+
+            members.Add(new Models.GroupModel { StudentNumber = "u01234567", FirstName = "Jane", LastName = "Doe", EmailAddress = "jane.doe@tuks.co.za" });
+            members.Add(new Models.GroupModel { StudentNumber = "u12345678", FirstName = "John", LastName = "Doe", EmailAddress = "john.doe@tuks.co.za" });
+            members.Add(new Models.GroupModel { StudentNumber = "u23456789", FirstName = "Jeff", LastName = "Winters", EmailAddress = "jeff.winters@tuks.co.za" });
+            members.Add(new Models.GroupModel { StudentNumber = "u76543210", FirstName = "Jack", LastName = "Hugo", EmailAddress = "jack.hugo@tuks.co.za" });
+            members.Add(new Models.GroupModel { StudentNumber = "u87654321", FirstName = "Jenna", LastName = "White", EmailAddress = "jenna.white@tuks.co.za" });
+
+            return View(members);
+
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lukasStudyGroup.MvcApplication" Language="C#" %>
