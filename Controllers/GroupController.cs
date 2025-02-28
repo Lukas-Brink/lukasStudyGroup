05a32d919e035d9b99c427b3555fc60e4fd3ab6a
Lukas-Brink <u24616185@tuks.co.za>
2025-02-28 15:42:31 +0200

    test

--- Controllers/GroupController.cs
+++ Controllers/GroupController.cs
@@ -11,11 +11,11 @@ namespace lukasStudyGroup.Controllers
         {
             List<Models.GroupModel> members = new List<Models.GroupModel>();
 
-            members.Add(new Models.GroupModel { StudentNumber = "u01234567", FirstName = "Jane", LastName = "Doe", EmailAddress = "jane.doe@tuks.co.za" });
-            members.Add(new Models.GroupModel { StudentNumber = "u12345678", FirstName = "John", LastName = "Doe", EmailAddress = "john.doe@tuks.co.za" });
-            members.Add(new Models.GroupModel { StudentNumber = "u23456789", FirstName = "Jeff", LastName = "Winters", EmailAddress = "jeff.winters@tuks.co.za" });
-            members.Add(new Models.GroupModel { StudentNumber = "u76543210", FirstName = "Jack", LastName = "Hugo", EmailAddress = "jack.hugo@tuks.co.za" });
-            members.Add(new Models.GroupModel { StudentNumber = "u87654321", FirstName = "Jenna", LastName = "White", EmailAddress = "jenna.white@tuks.co.za" });
+            members.Add(new Models.GroupModel { StudentNumber = "u24616185", FirstName = "Lukas", LastName = "Brink", EmailAddress = "u24616185@tuks.co.za" });
+            members.Add(new Models.GroupModel { StudentNumber = "u24607364", FirstName = "Helena", LastName = "van der Merwe", EmailAddress = "u24607364@tuks.co.za" });
+            members.Add(new Models.GroupModel { StudentNumber = "u24647502", FirstName = "Kelly", LastName = "Johnson", EmailAddress = "u24647502@tuks.co.za" });
+            members.Add(new Models.GroupModel { StudentNumber = "u24679438", FirstName = "Rayhan", LastName = "Ounaceur", EmailAddress = "u24679438@tuks.co.za" });
+            members.Add(new Models.GroupModel { StudentNumber = "u24616827", FirstName = "Luan", LastName = "Fourie", EmailAddress = "u24616827@tuks.co.za" });
 
             return View(members);
 
